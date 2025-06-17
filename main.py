03d0527909a134e20657e217d031f27bf3166df4
Sammy <bavesh2006kumar@gmail.com>
2025-06-17 12:02:41 +0000

    /

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import cv2
 import io
 from PIL import Image
 from google.colab import files
+
+img=cv2.imread('pup.jpg')
+ # pre-processing image
+ # Converting to greyscale
+Grey_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
