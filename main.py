f94d26fdaa2580f3264fd25b3dfcf7430051a61c
Sammy <bavesh2006kumar@gmail.com>
2025-06-19 12:47:31 +0000

    .

--- main.py
+++ main.py
@@ -11,3 +11,6 @@ img=cv2.imread('pup.jpg')
 Grey_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 cv2_imshow(Grey_img)# to see grey image
+
+grey_blur=cv2.medianBlur(Grey_img,5)# adds blur effect to image
+cv2_imshow(grey_blur)#to see blur image
