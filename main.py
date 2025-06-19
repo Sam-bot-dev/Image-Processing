112aad21e809b10a748f10ed22e78a88ccb2c76c
Sammy <bavesh2006kumar@gmail.com>
2025-06-19 12:49:46 +0000

    .

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ grey_blur=cv2.medianBlur(Grey_img,5)# adds blur effect to image
 cv2_imshow(grey_blur)#to see blur image
 edges=cv2.Canny(grey_blur,100,200)#this will make the image black and will show only edges
 cv2_imshow(edges)
+edge=cv2.adaptiveThreshold(grey_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blockSize=9,C=9)
+cv2_imshow(edge)
+# Original image
+print("Original image")
+cv2_imshow(img)# to see original image
